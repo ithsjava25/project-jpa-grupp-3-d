f5e3512ac4a42a5e9c8f6de28dc158999b415363
Kristina M <kristina0x7@gmail.com>
2025-12-16 01:01:43 +0100

    Add RunInTransaction method to handle transactional operations

--- src/main/java/org/example/repository/BaseRepository.java
+++ src/main/java/org/example/repository/BaseRepository.java
@@ -1,6 +1,9 @@
 package org.example.repository;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.function.Function;
 
 public abstract class BaseRepository <T, ID> {
 
@@ -11,4 +14,20 @@ public abstract class BaseRepository <T, ID> {
         this.emf = emf;
         this.entityClass = entityClass;
     }
+
+    protected <R> R runInTransaction(Function<EntityManager, R> action) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            R result = action.apply(em);
+            em.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw new RuntimeException("Transaction failed for " + entityClass.getSimpleName(), e);
+        } finally {
+            em.close();
+        }
+    }
+
 }
