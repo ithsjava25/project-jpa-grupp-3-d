dd23f32263b4399f13d710da2f8d603b7e000947
Annika Holmqvist <annika.holmqvist94@gmail.com>
2025-12-17 12:41:33 +0100

    Add `InvoiceItemRepository` extending `BaseRepository` with `create` and `update` methods

---
+++ src/main/java/org/example/repository/InvoiceItemRepository.java
@@ -0,0 +1,29 @@
+package org.example.repository;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.example.entity.InvoiceItem;
+
+import java.util.UUID;
+
+public class InvoiceItemRepository extends BaseRepository<InvoiceItem, UUID> {
+    protected InvoiceItemRepository(EntityManagerFactory emf, Class<InvoiceItem> entityClass) {
+        super(emf, entityClass);
+    }
+
+    public InvoiceItem create(InvoiceItem invoiceItem) {
+        return runInTransaction(em -> {
+            em.persist(invoiceItem);
+            return invoiceItem;
+        });
+    }
+
+    public InvoiceItem update(InvoiceItem invoiceItem) {
+        return runInTransaction(em -> {
+            em.merge(invoiceItem);
+            return invoiceItem;
+        });
+    }
+
+
+
+}
