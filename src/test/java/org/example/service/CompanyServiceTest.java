5709211e1cd6284772c40dd1bae83d6b115c9b36
Kristina M <kristina0x7@gmail.com>
2025-12-28 15:15:03 +0100

    add CompanyServiceTest class with test methods

---
+++ src/test/java/org/example/service/CompanyServiceTest.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void toDto() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteCompany() {
+    }
+}
